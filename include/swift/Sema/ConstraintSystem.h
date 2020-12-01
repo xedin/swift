d9d31367ce70cc3bdb878574c9a737f726af9137
Pavel Yaskevich <pyaskevich@apple.com>
2020-12-01 15:34:17 -0800

    [ConstraintSystem] NFC: Extract `requiresOptionalAdjustment` so it could be used for default bindings

--- include/swift/Sema/ConstraintSystem.h
+++ include/swift/Sema/ConstraintSystem.h
@@ -5830,6 +5830,10 @@ private:
   /// \returns true if some new bindings were sucessfully computed,
   /// false otherwise.
   bool computeNext();
+
+  /// Check whether binding type is required to either conform to
+  /// `ExpressibleByNilLiteral` protocol or be wrapped into an optional type.
+  bool requiresOptionalAdjustment(const Binding &binding) const;
 };
 
 /// Iterator over disjunction choices, makes it
